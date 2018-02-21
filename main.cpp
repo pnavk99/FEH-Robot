880372603b6b4ae7b96263763fef59bc9c6259b9
pnavk99 <31644285+pnavk99@users.noreply.github.com>
2018-02-20 19:04:01 -0500

    Bump switch and photoresistor code added

--- main.cpp
+++ main.cpp
@@ -9,11 +9,18 @@
 #define PI 3.141593
 
 //Declarations for encoders & motors
+AnalogInputPin lightSensor(FEHIO::P0_4);
+
+DigitalInputPin frontBump(FEHIO::P1_0);
+
 DigitalEncoder right_encoder(FEHIO::P0_0);
 DigitalEncoder left_encoder(FEHIO::P0_1);
+
 FEHMotor right_motor(FEHMotor::Motor0,9.0);
 FEHMotor left_motor(FEHMotor::Motor1,9.0);
 
+
+//Using shaft encoders
 void move_forward(int percent, float distance) //using encoders
 {
     int counts = distance*318/(2*PI*1.25);
@@ -55,20 +62,31 @@ void rotate(int angle,int direction, int percent)
     right_motor.Stop();
 }
 
+//Using bump switch
+void bump_move(int percent)
+{
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+
+    while(frontBump.Value());
+
+    left_motor.Stop();
+    right_motor.Stop();
+}
+
 int main(void)
 {
     int motor_percent = 30; //Input power level here
+    float threshold = 0.6;
 
-    float x, y; //for touch screen
-        
     //Initialize the screen
     LCD.Clear(BLACK);
     LCD.SetFontColor(WHITE);
 
-    LCD.WriteLine("Shaft Encoder Exploration Test");
-    LCD.WriteLine("Touch the screen");
-    while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
-    while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
+    LCD.WriteLine("Until the light shines bright,");
+    LCD.WriteLine("I shall not do shyte");
+
+    while(lightSensor.Value()>threshold);
 
     //Insert movement code (function calls)
 
